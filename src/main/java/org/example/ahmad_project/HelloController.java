e43b1937fa330ee941d3218d8413e7f14f81477f
AhmadMayyaleh122 <mayahmad336@gmail.com>
2025-03-24 16:33:21 +0200

    test 1

--- src/main/java/org/example/ahmad_project/HelloController.java
+++ src/main/java/org/example/ahmad_project/HelloController.java
@@ -4,8 +4,8 @@ import javafx.fxml.FXML;
 import javafx.scene.control.Label;
 
 public class HelloController {
-    @FXML
-    private Label welcomeText;
+//    @FXML
+//    private Label welcomeText;
 
     @FXML
     protected void onHelloButtonClick() {
