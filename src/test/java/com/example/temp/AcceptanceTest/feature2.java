c94bc280966720c8d0abfd15aca6f651b4f112ff
yazee <yazeed.k2016@gmail.com>
2025-03-28 22:20:10 +0200

    Add files via upload

---
+++ src/test/java/com/example/temp/AcceptanceTest/feature2.java
@@ -0,0 +1,49 @@
+package com.example.temp.AcceptanceTest;
+
+import io.cucumber.java.en.Given;
+import io.cucumber.java.en.Then;
+import io.cucumber.java.en.When;
+import io.cucumber.junit.Cucumber;
+import io.cucumber.junit.CucumberOptions;
+import io.cucumber.junit.CucumberOptions.SnippetType;
+import org.junit.runner.RunWith;
+import static org.junit.Assert.assertArrayEquals;
+
+public class feature2 {
+
+  @Given("the customer is on the custom meal selection page")
+  public void theCustomerIsOnTheCustomMealSelectionPage() {
+
+  }
+    @When("the customer selects ingredients from the available list")
+    public void theCustomerSelectsIngredientsFromTheAvailableList() {
+
+    }
+    @Then("the system should validate the ingredient combinations")
+    public void theSystemShouldValidateTheIngredientCombinations() {
+
+    }
+    @Then("the system should allow the customer to proceed with the order")
+    public void theSystemShouldAllowTheCustomerToProceedWithTheOrder() {
+
+    }
+
+    @Given("the customer is on the custom meal selection page")
+    public void theCustomerIsOnTheCustom_MealSelectionPage() {
+
+    }
+    @When("the customer selects unvailable or incompativle ingredient")
+    public void theCustomerSelectsUnvailableOrIncompativleIngredient() {
+
+    }
+    @Then("the system should display an error message")
+    public void theSystemShouldDisplayAnErrorMessage() {
+
+    }
+    @Then("the system should not allow the customerr to procced")
+    public void theSystemShouldNotAllowTheCustomerrToProcced() {
+
+    }
+
+
+}
