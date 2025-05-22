f4efee5f48f2e1a18ff1839d3db21297f7f95597
yazee <yazeed.k2016@gmail.com>
2025-05-22 14:31:09 +0200

    hi

--- src/main/java/com/example/cooking_proj/Create_Custome_Meal_2.java
+++ src/main/java/com/example/cooking_proj/Create_Custome_Meal_2.java
@@ -54,62 +54,93 @@ public class Create_Custome_Meal_2 {
         return suggestedAlternative;
     }
 
-    public List<List> create_meal(List<String> Preference, List<String> Allergies) {
+    public List<List> create_meal(List<String> preference, List<String> allergies) {
         Stock = stockSystem.getStock();
+        Map.Entry<List<String>, List<Integer>> stockMap = Stock.entrySet().iterator().next();
+        ingredients = stockMap.getKey();
+        quantity = stockMap.getValue();
 
-        Map.Entry<List<String>, List<Integer>> StockMap = Stock.entrySet().iterator().next();
-        ingredients = StockMap.getKey();
-        quantity = StockMap.getValue();
-
-        this.selectedAllegries = Allergies;
-        this.selectedIngredients = Preference;
+        this.selectedAllegries = allergies;
+        this.selectedIngredients = preference;
 
         Set<String> seen = new HashSet<>();
+        handleUnavailableOrAllergicIngredients(seen);
+        handleIncompatibleIngredients();
+        handleEmptyStorageItems();
 
+        for (String s : selectedIngredients) {
+            stockSystem.decrease_quantity(s);
+        }
+
+        create_meal.add(selectedIngredients);
+        create_meal.add(selectedAllegries);
+        return create_meal;
+    }
+
+    private void handleUnavailableOrAllergicIngredients(Set<String> seen) {
+        for (int i = 0; i < selectedIngredients.size(); i++) {
+            String ingredient = selectedIngredients.get(i);
+
+            if (isUnavailable(ingredient, seen)) {
+                replaceAndNotifyUnavailable(ingredient, seen);
+            }
+
+            if (isAllergic(ingredient)) {
+                replaceAndNotifyAllergy(ingredient);
+            }
+        }
+    }
+
+    private boolean isUnavailable(String ingredient, Set<String> seen) {
+        return !ingredients.contains(ingredient) && !seen.contains(ingredient);
+    }
+
+    private void replaceAndNotifyUnavailable(String ingredient, Set<String> seen) {
+        seen.add(ingredient);
+        suggestedAlternative = suggest_alternative(ingredient);
+        replaceIngredient(ingredient, suggestedAlternative);
+        System.out.println("Sorry, we don't have " + ingredient + ", so the alternative will be " + suggestedAlternative);
+    }
+
+    private boolean isAllergic(String ingredient) {
+        return selectedAllegries.contains(ingredient);
+    }
+
+    private void replaceAndNotifyAllergy(String ingredient) {
+        suggestedAlternative = suggest_alternative(ingredient);
+        replaceIngredient(ingredient, suggestedAlternative);
+        System.out.println("Warning: " + ingredient + " is in the allergy list, so the alternative will be " + suggestedAlternative);
+    }
+
+    private void handleIncompatibleIngredients() {
         for (int i = 0; i < selectedIngredients.size(); i++) {
             String ingredient1 = selectedIngredients.get(i);
-
-            if (!ingredients.contains(ingredient1) && !seen.contains(ingredient1)) {
-                seen.add(ingredient1);
-                suggestedAlternative = suggest_alternative(ingredient1);
-                selectedIngredients.remove(ingredient1);
-                selectedIngredients.add(suggestedAlternative);
-                System.out.println("Sorry, we don't have " + ingredient1 + ", so the alternative will be " + suggestedAlternative);
-
-            }
-
-            if (selectedAllegries.contains(ingredient1)) {
-                suggestedAlternative = suggest_alternative(ingredient1);
-                selectedIngredients.remove(ingredient1);
-                selectedIngredients.add(suggestedAlternative);
-                System.out.println("Warning: " + ingredient1 + " is in the allergy list, so the alternative will be " + suggestedAlternative);
-            }
 
             for (int j = i + 1; j < selectedIngredients.size(); j++) {
                 String ingredient2 = selectedIngredients.get(j);
 
-                if (incompatiblePairs.containsKey(ingredient1)) {
-                    String incompatibleWith = incompatiblePairs.get(ingredient1);
-                    if (incompatibleWith.equals(ingredient2)) {
-                        suggestedAlternative = suggest_alternative(ingredient1);
-                        selectedIngredients.remove(ingredient1);
-                        selectedIngredients.add(suggestedAlternative);
-                        System.out.println("Incompatible Pair Found: " + ingredient1 + " and " + ingredient2 +
-                                ". Suggested alternative: " + suggestedAlternative);
-                    }
+                if (isIncompatible(ingredient1, ingredient2)) {
+                    suggestedAlternative = suggest_alternative(ingredient1);
+                    replaceIngredient(ingredient1, suggestedAlternative);
+                    System.out.println("Incompatible Pair Found: " + ingredient1 + " and " + ingredient2 +
+                            ". Suggested alternative: " + suggestedAlternative);
                 }
             }
         }
+    }
 
-        for(String s: selectedIngredients) {
-            stockSystem.Check_quantity(s);
-        }
+    private boolean isIncompatible(String ing1, String ing2) {
+        return incompatiblePairs.containsKey(ing1) && incompatiblePairs.get(ing1).equals(ing2);
+    }
 
-        Iterator<String> iterator = selectedIngredients.iterator();
+    private void handleEmptyStorageItems() {
         List<String> updatedIngredients = new ArrayList<>();
+        Iterator<String> iterator = selectedIngredients.iterator();
 
         while (iterator.hasNext()) {
             String s = iterator.next();
+            stockSystem.Check_quantity(s);
+
             if (stockSystem.is_empty_storage(s)) {
                 suggestedAlternative = suggest_alternative(s);
                 System.out.println("Sorry Because We Don't Have " + s + " So Your Alternative Will Be " + suggestedAlternative);
@@ -119,16 +150,13 @@ public class Create_Custome_Meal_2 {
         }
 
         selectedIngredients.addAll(updatedIngredients);
-
-        for (String s : selectedIngredients) {
-            stockSystem.decrease_quantity(s);
-        }
-
-        create_meal.add(selectedIngredients);
-        create_meal.add(selectedAllegries);
-
-        return create_meal;
     }
+
+    private void replaceIngredient(String oldIng, String newIng) {
+        selectedIngredients.remove(oldIng);
+        selectedIngredients.add(newIng);
+    }
+
 
     public List<String>GetMeals(){
         return Meals;
